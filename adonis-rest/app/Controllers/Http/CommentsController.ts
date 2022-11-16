13c82bc2df21b98105ae756cfd3f3cfb0f916085
Vinicius Morgado <vinicius_donatto@outlook.com>
2022-11-16 04:44:05 -0300

    Implements to CommentsController Delete and Update methods

--- adonis-rest/app/Controllers/Http/CommentsController.ts
+++ adonis-rest/app/Controllers/Http/CommentsController.ts
@@ -25,13 +25,23 @@ export default class CommentsController {
       data: comment
     }
   }
-  /*
-  public async destroy({params}: HttpContextContract) {
 
+  public async destroy({params}: HttpContextContract) {
+    const comment = await Comment.findOrFail(params.id);
+    await comment.delete();
+    return {
+      message: `O item ${comment.id} foi deletado com sucesso!`
+    }
   }
 
   public async update({params, request}: HttpContextContract){
-
-
-  }*/
+    const comment = await Comment.findOrFail(params.id);
+    const body = request.body();
+    comment.title = body.title;
+    comment.text = body.text;
+    await comment.save();
+    return {
+      message: `O comentário ${comment.id} foi atualizado com sucesso!`
+    }
+  }
 }
