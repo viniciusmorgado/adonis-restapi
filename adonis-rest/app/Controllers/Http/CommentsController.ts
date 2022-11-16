790479594d1df1d38d576410811c6c21f7b0eac3
Vinicius Morgado <vinicius_donatto@outlook.com>
2022-11-16 04:17:05 -0300

    Implements to CommentsController get methods

--- adonis-rest/app/Controllers/Http/CommentsController.ts
+++ adonis-rest/app/Controllers/Http/CommentsController.ts
@@ -3,34 +3,35 @@ import Comment from "App/Models/Comment";
 
 export default class CommentsController {
   public async store({ request, response }: HttpContextContract) {
-
     const body = request.body();
     const comment = await Comment.create(body);
     response.status(200);
-
     return {
       message: 'Comentário registrado com sucesso!',
       data: comment
     }
   }
-}
 
-/*
   public async index() {
-
+    const comment = await Comment.all();
+    return {
+      data: comment
+    }
   }
 
   public async show({params}: HttpContextContract) {
-
+    const comment = await Comment.findOrFail(params.id);
+    return {
+      data: comment
+    }
   }
-
+  /*
   public async destroy({params}: HttpContextContract) {
 
   }
 
-  public async update({params, request}: HttpContextContract){*/
-/*
+  public async update({params, request}: HttpContextContract){
 
-  }
+
+  }*/
 }
-*/
