6659b7fd49559b9554b676443363730f69e23e06
Vinicius Morgado <vinicius_donatto@outlook.com>
2022-11-16 00:51:40 -0300

    MementsController Refactor with a new route

--- adonis-rest/app/Controllers/Http/MomentsController.ts
+++ adonis-rest/app/Controllers/Http/MomentsController.ts
@@ -2,7 +2,13 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Moment from 'App/Models/Moment'
 
 export default class MomentsController {
-  public async store() {
-
+  public async store({ request, response }: HttpContextContract) {
+    const body = request.body();
+    const moment = await Moment.create(body);
+    response.status(201);
+    return {
+      message: 'Momento criado com sucesso!',
+      data: moment
+    }
   }
 }
