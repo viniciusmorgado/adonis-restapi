b94a4bb9e523c33e19c302cce2d0fc7636aa9a53
Vinicius Morgado <vinicius_donatto@outlook.com>
2022-11-16 00:31:33 -0300

    Add database content and MomentController

---
+++ adonis-rest/app/Controllers/Http/MomentsController.ts
@@ -0,0 +1,8 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Moment from 'App/Models/Moment'
+
+export default class MomentsController {
+  public async store() {
+
+  }
+}
